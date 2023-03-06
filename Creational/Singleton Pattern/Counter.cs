39acc1c7b3384bf55719ffd6d5e849ec3bcf9fed
Mahmoud Kassem <modykassem123@gmail.com>
2023-03-06 20:36:05 +0200

    Add project files.

---
+++ Creational/Singleton Pattern/Counter.cs
@@ -0,0 +1,27 @@
+﻿namespace Singleton_Pattern;
+
+public class Counter
+{
+    private Counter()
+    {
+
+    }
+    static Counter? counter = null;
+    private static object lockObj = new object();
+    public int Count { get; set; }
+    public void AddOne() => Count++;
+
+    public static Counter GetInistance()
+    {
+        // lazy intialization
+        if (counter is null)
+        {
+            lock (lockObj)
+            {
+                if (counter is null) counter = new Counter();
+            }
+        }
+
+        return counter;
+    }
+}
---
+++ Creational/Singleton Pattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using Singleton_Pattern;
+
+Counter counter3 = Counter.GetInistance();
+
+await Task.Factory.StartNew(() =>
+{
+    Counter counter1 = Counter.GetInistance();
+    counter1.AddOne();
+    Console.WriteLine(counter1.Count);
+});
+await Task.Factory.StartNew(() =>
+{
+    Counter counter2 = Counter.GetInistance();
+    counter2.AddOne();
+    Console.WriteLine(counter2.Count);
+});
+
+//Console.WriteLine(counter1.Count);
+//Console.WriteLine(counter2.Count);
+//Console.WriteLine(counter3.Count);
